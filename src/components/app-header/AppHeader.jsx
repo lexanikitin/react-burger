5e396c9ee56ec2ebe47cc33d1866d190c7646d85
Alexey Nikitin <lexa.nikitin@gmail.com>
2022-12-30 11:10:28 +0300

    bugfix: элементы кнопок в хедере заменены на ссылки

--- src/components/app-header/AppHeader.jsx
+++ src/components/app-header/AppHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Logo, Button, BurgerIcon, ProfileIcon, ListIcon} from "@ya.praktikum/react-developer-burger-ui-components";
+import {Logo, BurgerIcon, ProfileIcon, ListIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 import app_header from './app-header.module.css';
 import clsx from "clsx";
 
@@ -8,26 +8,26 @@ const AppHeader = () => {
   return (<header className={app_header.header}>
     <div className={clsx(app_header.content, 'pt-4', 'pb-4')}>
       <nav className={clsx(app_header.nav_wrapper)}>
-        <Button extraClass={clsx('pl-5', 'pr-5', 'pt-4', 'pb-4', app_header.button)} htmlType="button"
-                type="secondary" size="medium">
+        <a href={'#'} className={clsx('pl-5', 'pr-5', 'pt-4', 'pb-4', app_header.button)}
+        >
           <BurgerIcon type="primary"/>
           <span
             className={clsx('pl-2', 'text', 'text_type_main-default', app_header.button_text)}>Конструктор</span>
-        </Button>
-        <Button extraClass={clsx('pl-5', 'pr-5', 'pt-4', 'pb-4', app_header.button)} htmlType="button"
-                type="secondary" size="medium" disabled={true}>
+        </a>
+        <a href={'#'} className={clsx('pl-5', 'pr-5', 'pt-4', 'pb-4', app_header.button)}
+        >
           <ListIcon type="secondary"/>
-          <span className={'pl-2'}>Лента заказов</span>
-        </Button>
+          <span className={clsx('pl-2', 'text_color_inactive')}>Лента заказов</span>
+        </a>
       </nav>
       <div className={app_header.logo_wrapper}>
         <Logo/>
       </div>
-      <Button extraClass={clsx('pl-5', 'pr-5', 'pt-4', 'pb-4', app_header.button)} htmlType="button"
-              type="secondary" size="medium" disabled={true}>
+      <a href={'#'} className={clsx('pl-5', 'pr-5', 'pt-4', 'pb-4', app_header.button)}
+      >
         <ProfileIcon type="secondary"/>
-        <span className={'pl-2'}>Личный кабинет</span>
-      </Button>
+        <span className={clsx('pl-2', 'text_color_inactive')}>Личный кабинет</span>
+      </a>
     </div>
   </header>);
 };
