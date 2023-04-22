e8dd9b34fc35348e19e66c77b68f5bb4289cbb78
Alexey Nikitin <lexa.nikitin@gmail.com>
2023-04-22 18:54:52 +0300

    feat: типизация компонента BurgerIngredient

--- src/components/burger-ingredient/BurgerIngredient.tsx
+++ src/components/burger-ingredient/BurgerIngredient.tsx
@@ -1,19 +1,24 @@
-import React, {useMemo} from 'react';
+import React, {FC, useMemo} from 'react';
 import {Counter, CurrencyIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 import cardStyles from './burger-ingredient.module.css'
 import clsx from "clsx";
 import {useDispatch, useSelector} from "react-redux";
 import {MODAL_SET_CURRENT_INGREDIENT} from "../../services/actions/modal";
 import {useDrag} from "react-dnd";
+import {TBurgerIngredientInfo} from "../../utils/types";
 
-const BurgerIngredient = ({info, setModalActive}) => {
+type TBurgerIngredient = {
+  info: TBurgerIngredientInfo;
+  setModalActive: Function;
+}
+const BurgerIngredient: FC<TBurgerIngredient> = ({info, setModalActive}) => {
   const dispatch = useDispatch();
-  const id = info._id;
+  //@ts-ignore
   const {selectedIngredients, selectedBun} = useSelector(store => store.order);
-  const count = useMemo(() => {
-    return [selectedIngredients, selectedBun].flat().filter(item => item._id === info._id).length
+  const count = useMemo<number | undefined>(() => {
+    return [selectedIngredients, selectedBun].flat().filter((item: TBurgerIngredientInfo) => item._id === info._id).length
   }, [selectedIngredients, selectedBun]);
-  const [{ isDrag }, drag] = useDrag({
+  const [{isDrag}, drag] = useDrag({
     type: "ingredient",
     item: {info},
     collect: monitor => ({
