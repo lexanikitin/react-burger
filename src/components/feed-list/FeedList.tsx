7ccd86040b968a785a692ce1535e8cf1963d97d7
Alexey Nikitin <lexa.nikitin@gmail.com>
2023-04-23 17:18:07 +0300

    feat: типизация функционального компонента FeedList

--- src/components/feed-list/FeedList.tsx
+++ src/components/feed-list/FeedList.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import clsx from "clsx";
 import styles from "./feed-list.module.css";
 import Modal from "../modal/Modal";
@@ -7,12 +7,14 @@ import OrderCard from "../order-card/OrderCard";
 import OrderDetails from "../order-details/OrderDetails";
 import {FEED_CLEAR_CURRENT_ORDER, FEED_SET_CURRENT_ORDER} from "../../services/actions/feed";
 import {useLocation} from "react-router-dom";
+import {TOrder} from "../../utils/types";
 
-const FeedList = () => {
+const FeedList: FC = () => {
   const dispatch = useDispatch();
   const location = useLocation();
 
-  const [isModalActive, setModalActive] = useState(false);
+  const [isModalActive, setModalActive] = useState<boolean>(false);
+  //@ts-ignore
   const {orders} = useSelector(store => store.feed);
   useEffect(() => {
     if (isModalActive === false) {
@@ -35,8 +37,8 @@ const FeedList = () => {
 
   return (
     <section className={clsx(styles.section)}>
-      <ul className={clsx(styles.list)} >
-        {orders.map((item, index) => {
+      <ul className={clsx(styles.list)}>
+        {orders.map((item:TOrder, index:number) => {
           return (
             <OrderCard key={index} order={item} setModalActive={setModalActive}/>
           );
